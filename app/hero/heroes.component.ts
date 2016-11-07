0aadd52e2d3c086b1c849724b21c7e99d65b580b
mybrc91 <gdzrch@163.com>
2016-11-07 11:31:17 +0800

    update

--- app/hero/heroes.component.ts
+++ app/hero/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Hero,HeroService } from './hero.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   //providers : [HeroService],		
@@ -17,7 +17,7 @@ export class HeroesComponent implements OnInit{
 	}
 
 	constructor(
-		private router: Router,
+		private router: ActivatedRoute,
 		private heroService :HeroService){};
 
 	add(name: String):void{
@@ -47,7 +47,7 @@ export class HeroesComponent implements OnInit{
   	}
 
   	gotoDetail(): void{
-  		this.router.navigate(['/detail' , this.selectedHero.id]);
+  		//this.router.navigate(['/detail' , this.selectedHero.id]);
   	}
 
 	title = 'Tour of Heroes';
