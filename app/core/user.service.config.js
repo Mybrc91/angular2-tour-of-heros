d4f2aab6341868e3b8c64c48a1d603ef53e159b8
mybrc91 <gdzrch@163.com>
2016-11-08 11:55:12 +0800

    update igonre

--- app/core/user.service.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var UserServiceConfig = (function () {
-    function UserServiceConfig() {
-        this.userName = '';
-    }
-    return UserServiceConfig;
-}());
-exports.UserServiceConfig = UserServiceConfig;
-//# sourceMappingURL=user.service.config.js.map
